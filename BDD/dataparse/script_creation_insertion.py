e78dc01f6bc65d5d9cdfb3dc073d7932978ffe57
RaavenBZH <emalivet56@gmail.com>
2022-04-10 21:50:57 +0200

    Modifs

--- BDD/dataparse/script_creation_insertion.py
+++ BDD/dataparse/script_creation_insertion.py
@@ -2,40 +2,59 @@ import chouettes
 
 chouette = chouettes.parse((r'C:\Users\emali\OneDrive\Documents\Work\Bac+1\S2\S2.01\sql\SAE-PNR\BDD\dataparse\Suivi_chouettes\Chouettes_Point_Ecoute_2019.dbf', r'C:\Users\emali\OneDrive\Documents\Work\Bac+1\S2\S2.01\sql\SAE-PNR\BDD\dataparse\Suivi_chouettes\Chouettes_Point_Individus_2019.dbf'))
 
-f = open("insert.sql", "w")
+f = open("insertChouette.sql", "w")
 
-f.write("-- Insertions Chouette\n")
+f.write("\n-- Insertions Lieu\n")
+for i in range(len(chouette[0])):
+    dic = chouette[0][i]
+    d = dic['coord_Lambert_X']
+    e = dic['coord_Lambert_Y']
+    if d == 0:
+        d = 'NULL'
+    if e == 0:
+        e = 'NULL'
+    f.write(f"INSERT INTO Lieu VALUES({d},{e});\n")
+
+f.write("\n-- Insertions Observation\n")
+for i in range(len(chouette[0])):
+    dic = chouette[0][i]
+    a = i+1
+    b = "TO_DATE(\'01/01/" + str(dic['date']) +  "\', \'dd/mm/yyyy')" if dic['date'] != 0 else "NULL"
+    #b = "01/01/" + str(dic['date']) if dic['date'] != 0 else "NULL"
+    c = 'NULL'
+    d = dic['coord_Lambert_X']
+    e = dic['coord_Lambert_Y']
+    if b == 0:
+        b = 'NULL'
+    if d == 0:
+        d = 'NULL'
+    if e == 0:
+        e = 'NULL'
+    f.write(f"INSERT INTO Observation VALUES({a},{b},{c},{d},{e});\n")
+
+f.write("\n-- Insertions Chouette\n")
 for dic in chouette[0]:
-
     a = dic['numIndividu']
     b = dic['espece']
-    c = dic['sexe']
-        
+    c = dic['sexe'] 
     if a != 'NULL':
         a = "\'" + a + "\'"
-
     if b != 'NULL':
         b = "\'" + b + "\'"
-    
     if c != 'NULL':
         c = "\'" + c + "\'"
-
     f.write(f"INSERT INTO Chouette VALUES({a},{b},{c});\n")
 
 f.write("\n-- Insertions Obs_Chouette\n")
-
 for i in range(len(chouette[0])):
-
     dic = chouette[0][i]
     a = i+1
     b = dic['protocole']
     c = dic['typeObs']
     d = dic['numIndividu']
-
     if c != 'NULL':
         c = "\'" + c + "\'"
-
     if d != 'NULL':
         d = "\'" + d + "\'"
-
     f.write(f"INSERT INTO Obs_Chouette VALUES({a},{b},{c},{d});\n")
+
