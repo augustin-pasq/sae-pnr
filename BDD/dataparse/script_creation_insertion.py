94b684370bfdb28cc8b7a79384a78ac90a4bf214
finxol <personnal@bottleneck.anonaddy.com>
2022-04-10 22:29:34 +0200

    ajout insertion QuiObserveChouette

--- BDD/dataparse/script_creation_insertion.py
+++ BDD/dataparse/script_creation_insertion.py
@@ -1,6 +1,7 @@
 import chouettes
 
-chouette = chouettes.parse((r'C:\Users\emali\OneDrive\Documents\Work\Bac+1\S2\S2.01\sql\SAE-PNR\BDD\dataparse\Suivi_chouettes\Chouettes_Point_Ecoute_2019.dbf', r'C:\Users\emali\OneDrive\Documents\Work\Bac+1\S2\S2.01\sql\SAE-PNR\BDD\dataparse\Suivi_chouettes\Chouettes_Point_Individus_2019.dbf'))
+chouette = chouettes.parse(
+    (r'Suivi_chouettes/Chouettes_Point_Ecoute_2019.dbf', r'Suivi_chouettes/Chouettes_Point_Individus_2019.dbf'))
 
 f = open("insertChouette.sql", "w")
 
@@ -18,8 +19,8 @@ for i in range(len(chouette[0])):
 f.write("\n-- Insertions Observation\n")
 for i in range(len(chouette[0])):
     dic = chouette[0][i]
-    a = i+1
-    b = "TO_DATE(\'01/01/" + str(dic['date']) +  "\', \'dd/mm/yyyy')" if dic['date'] != 0 else "NULL"
+    a = i + 1
+    b = "TO_DATE(\'01/01/" + str(dic['date']) + "\', \'dd/mm/yyyy')" if dic['date'] != 0 else "NULL"
     c = 'NULL'
     d = dic['coord_Lambert_X']
     e = dic['coord_Lambert_Y']
@@ -35,7 +36,7 @@ f.write("\n-- Insertions Chouette\n")
 for dic in chouette[0]:
     a = dic['numIndividu']
     b = dic['espece']
-    c = dic['sexe'] 
+    c = dic['sexe']
     if a != 'NULL':
         a = "\'" + a + "\'"
     if b != 'NULL':
@@ -47,7 +48,7 @@ for dic in chouette[0]:
 f.write("\n-- Insertions Obs_Chouette\n")
 for i in range(len(chouette[0])):
     dic = chouette[0][i]
-    a = i+1
+    a = i + 1
     b = dic['protocole']
     c = dic['typeObs']
     d = dic['numIndividu']
@@ -57,3 +58,11 @@ for i in range(len(chouette[0])):
         d = "\'" + d + "\'"
     f.write(f"INSERT INTO Obs_Chouette VALUES({a},{b},{c},{d});\n")
 
+
+f.write("\n-- Insertions QuiObserveChouette\n")
+for i in range(len(chouette[0])):
+    dic = chouette[0][i]
+    a = dic['idObs']
+    b = dic['observateur']
+    if a != 'NULL' and b != 'NULL':
+        f.write(f"INSERT INTO QuiObserveChouette VALUES({a},{b});\n")
