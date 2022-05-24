f7e8cff7c58f6bfe2cc505f9d7a263a08f49baa8
finxol <personnal@bottleneck.anonaddy.com>
2022-05-24 18:18:10 +0200

    add javadoc + various improvements

--- Java/src/modele/traitement/Graphe.java
+++ Java/src/modele/traitement/Graphe.java
@@ -1,99 +1,153 @@
 package modele.traitement;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
 /**
- * Graphe
+ * Class representing a graph
+ *
+ * @author Groupe SAE PNR 1D1
  */
 public class Graphe {
-
+    /**
+     * HashMap containing the nodes of the graph as keys and their neighbours as values
+     */
     private HashMap<Sommet, ArrayList<Sommet>> sommetsVoisins;
 
+    /**
+     * Construct a graph from a list of vertices and a minimal distance
+     * @param sommets List of vertices
+     * @param dist Minimal distance between two vertices to be neighbours
+     * @throws IllegalArgumentException If the list of vertices is null or if the minimal distance is negative
+     */
     public Graphe(ArrayList<Sommet> sommets, double dist) throws IllegalArgumentException {
         if (sommets == null) throw new IllegalArgumentException("sommets cannot be null");
+        else if (dist < 0) throw new IllegalArgumentException("dist cannot be negative");
         else {
             this.sommetsVoisins = new HashMap<Sommet, ArrayList<Sommet>>();
             for (Sommet s1 : sommets) {
-                ArrayList<Sommet> sv = new ArrayList<Sommet>();
+                ArrayList<Sommet> voisins = new ArrayList<Sommet>();
 
-                for (Sommet s2 : sommets) {
-                    if (s1.calculeDist(s2) > dist) {
-                        sv.add(s2);
-                    } else {
-                        sv = null;
-                    }
-                }
+                for (Sommet s2 : sommets)
+                    if (s1.calculeDist(s2) > dist)
+                        voisins.add(s2);
 
-                this.sommetsVoisins.put(s1, sv);
+                this.sommetsVoisins.put(s1, voisins);
             }
         }
     }
 
-    public Graphe(HashMap<Sommet, ArrayList<Sommet>> somVoisins) {
-        Set <Sommet> listeSommets = somVoisins.keySet();
-        for (Object o : listeSommets) if (o == null) throw new IllegalArgumentException("sommets cannot be null");
-        this.sommetsVoisins = somVoisins;
+    /**
+     * Construct a graph from a Hashmap containing the nodes of the graph as keys and their neighbours as values
+     * @param somVoisins Hashmap containing the nodes of the graph as keys and their neighbours as values
+     * @throws IllegalArgumentException If the Hashmap is null or if it contains null keys or null values
+     */
+    public Graphe(HashMap<Sommet, ArrayList<Sommet>> somVoisins) throws IllegalArgumentException {
+        if (somVoisins == null) throw new IllegalArgumentException("somVoisins cannot be null");
+        Set<Sommet> listeSommets = somVoisins.keySet();
+        for (Sommet s : listeSommets) {
+            if (s == null) throw new IllegalArgumentException("Hashmap key sommet cannot be null");
+            if (somVoisins.get(s) == null) throw new IllegalArgumentException("Hashmap value ArrayList cannot be null");
+        }
+        this.sommetsVoisins = new HashMap<Sommet, ArrayList<Sommet>>(somVoisins);
     }
 
+    /**
+     * Copy-constructor for a graph
+     * @param g Graph to be copied
+     * @throws IllegalArgumentException If the graph is null
+     */
     public Graphe(Graphe g) {
-        this.sommetsVoisins = g.sommetsVoisins;
+        this(g.getSommetsVoisins());
     }
 
+    /**
+     * Get the Hashmap containing the nodes and their neighbours
+     * @return Hashmap containing the nodes of the graph as keys and their neighbours as values
+     */
     public HashMap<Sommet, ArrayList<Sommet>> getSommetsVoisins() {
-        return this.sommetsVoisins;
+        return new HashMap<>(this.sommetsVoisins);
     }
 
+    /**
+     * Get the number of vertices in the graph
+     * @return Number of vertices in the graph
+     */
     public int nbSommets() {
         return this.sommetsVoisins.size();
     }
 
+    /**
+     * Get the number of edges in the graph
+     * @return Number of edges in the graph
+     */
     public int nbAretes() {
         int sum = 0;
         for (ArrayList<Sommet> s : this.sommetsVoisins.values()) {
-            sum = sum + s.size();
+            sum += s.size();
         }
-        return (int) (sum/2);
+        return sum / 2;
     }
 
-    public boolean estDansGraphe(int idSom){
+    /**
+     * Check if a vertex is in the graph
+     * @param idSom ID of the vertex to be checked
+     * @return True if the vertex is in the graph, false otherwise
+     */
+    public boolean estDansGraphe(int idSom) {
         boolean dansGraphe = false;
 
-        for (Sommet s : this.sommetsVoisins.keySet())
+        Iterator<Sommet> it = this.sommetsVoisins.keySet().iterator();
+
+        while (!dansGraphe && it.hasNext()) {
+            Sommet s = it.next();
             if (s.getId() == idSom)
                 dansGraphe = true;
+        }
 
         return dansGraphe;
     }
 
-    public int calculeDegree(int idSom){
+    /**
+     * Calculate the number of neighbours of a vertex
+     * @param idSom ID of the vertex
+     * @return Number of neighbours
+     */
+    public int calculeDegre(int idSom) {
         int degre = -1;
-        
+
         for (Sommet s : this.sommetsVoisins.keySet())
             if (s.getId() == idSom)
                 degre = this.sommetsVoisins.get(s).size();
 
         if (degre == -1)
-            throw new  IllegalArgumentException("id cannot be found");
+            throw new IllegalArgumentException("id cannot be found");
         else
             return degre;
     }
 
-    public HashMap<Sommet, Integer> calculeDegrees(){
+    /**
+     * Calculate the number of neighbours of all vertices in the graph
+     * @return Hashmap containing the ID of the vertex as key and the number of neighbours as value
+     */
+    public HashMap<Sommet, Integer> calculeDegres() {
         HashMap<Sommet, Integer> degres = new HashMap<Sommet, Integer>();
 
         for (Sommet s : this.sommetsVoisins.keySet())
-            degres.put(s, calculeDegree(s.getId()));
+            degres.put(s, calculeDegre(s.getId()));
 
         return degres;
     }
 
-    public Sommet somMaxDegree(){
-        HashMap<Sommet, Integer> degres = this.calculeDegrees();
+    /**
+     * Calculate the vertex with the most neighbours
+     * @return vertex with the most neighbours
+     */
+    public Sommet somMaxDegree() {
+        if (this.sommetsVoisins.isEmpty()) throw new IllegalArgumentException("Graph is empty");
+        HashMap<Sommet, Integer> degres = this.calculeDegres();
         Set<Sommet> listeSommets = degres.keySet();
         int maxDeg = -1;
-        Sommet sommet;
+        Sommet sommet = null;
 
         for (Sommet s : listeSommets)
             if (degres.get(s) > maxDeg){
@@ -104,7 +158,13 @@ public class Graphe {
         return sommet;
     }
 
-    public boolean sontVoisins(int idSom1, int idSom2){
+    /**
+     * Check if a vertex is a neighbour of another
+     * @param idSom1 ID of the first vertex
+     * @param idSom2 ID of the second vertex
+     * @return True if the second vertex is a neighbour of the first, false otherwise
+     */
+    public boolean sontVoisins(int idSom1, int idSom2) {
         boolean voisin = false;
 
         for (Sommet s1 : this.sommetsVoisins.keySet())
@@ -116,26 +176,22 @@ public class Graphe {
         return voisin;
     }
 
+    /**
+     * Generate the adjacency matrix of the graph
+     * @return Adjacency matrix of the graph
+     */
     public int[][] matriceAdjacence() {
-
-        // déclaration
-        int[][] adj;
-
-        nbSommets = this.nbSommets();
-        adj = new int[nbSommets][nbSommets + 1];
-        listeSommets = this.sommetsVoisins.keySet();
-        idSommets = new int[nbSommets];
-
-        for (Sommet s : this.sommetsVoisins.) {
-
-            adj[0][i] = this.sommetsVoisins.getKey(i);
-        }
-
-        // identifiant des sommets
-        for (i = 0; i < nbSommets; i++) adj[i][0] = idSommets[i];
+        // TODO : get the adjacency matrix
+        return new int[][]{new int[]{0}};
     }
 
-    public boolean existeChemin(int idSom1, int idSom2){
+    /**
+     * Check if a path exists between two vertices
+     * @param idSom1 ID of the first vertex
+     * @param idSom2 ID of the second vertex
+     * @return True if a path exists, false otherwise
+     */
+    public boolean existeChemin(int idSom1, int idSom2) {
         boolean chemin = false;
         ArrayList<Sommet> file = new ArrayList<Sommet>();
         ArrayList<Sommet> sommetsTraites = new ArrayList<Sommet>();
@@ -146,57 +202,84 @@ public class Graphe {
 
         // Ajout du premier sommet
         for (Sommet s1 : this.sommetsVoisins.keySet())
-            if (s1.getId() == idSom1){
+            if (s1.getId() == idSom1) {
                 s = s1;
             }
         sommetsTraites.add(s);
-        for (Sommet s1 : this.sommetsVoisins.get(s))
-            file.add(s1);
+        file.addAll(this.sommetsVoisins.get(s));
 
         // Vérification
-        while (file.size() > 0 && !chemin){
+        while (file.size() > 0 && !chemin) {
             s = file.remove(0);
             if (s.getId() == idSom2)
                 chemin = true;
             else
                 sommetsTraites.add(s);
-                for (Sommet s1 : this.sommetsVoisins.get(s))
-                    if (!file.contains(s1) && !sommetsTraites.contains(s1))
-                        file.add(s1);
+            for (Sommet s1 : this.sommetsVoisins.get(s))
+                if (!file.contains(s1) && !sommetsTraites.contains(s1))
+                    file.add(s1);
         }
+
         return chemin;
     }
 
-    public ArrayList<Sommet> voisins(int idSom) {
+    /**
+     * Get the neighbours of a vertex
+     * @param idSom ID of the vertex
+     * @return ArrayList containing the neighbours of the vertex
+     */
+    public ArrayList<Sommet> voisins(int idSom) throws IllegalArgumentException {
         ArrayList<Sommet> listeVoisins = null;
-        
-        if (!estDansGraphe(idSom))
-            throw new IllegalArgumentException("sommets cannot be null");
 
         for (Sommet s1 : this.sommetsVoisins.keySet())
             if (s1.getId() == idSom)
                 listeVoisins = this.sommetsVoisins.get(s1);
+
+        if (listeVoisins == null) throw new IllegalArgumentException("vertex is not in the graph");
+
         return listeVoisins;
     }
 
+    /**
+     * Calculate the smallest eccentricity of the graph
+     * @return Radius of the graph
+     */
     public int rayon() {
-        int s1;
-        int s2;
-        if (this.estConnexe()) {
-            return -1;
-        } else {
-
-        }
-
+        // TODO : determine the radius of the graph
+        return 0;
     }
 
-
+    /**
+     * Calculate the largest eccentricity of the graph
+     * @return Diameter of the graph
+     */
     public int diametre() {
-        if (this.estConnexe()) {
-            return -1;
-        } else {
+        // TODO : determine the diameter of the graph
+        return 0;
+    }
 
+    /**
+     * Check if the graph is connected
+     * @return True if the graph is connected, false otherwise
+     */
+    public boolean estConnexe() {
+        boolean connexe = true;
+        Set<Sommet> sommets = this.sommetsVoisins.keySet();
+        Iterator<Sommet> it = sommets.iterator();
+        Sommet s1;
+        Sommet s2;
+
+        while (it.hasNext() && connexe) {
+            s1 = it.next();
+
+            Iterator<Sommet> it2 = sommets.iterator();
+            while (it2.hasNext() && connexe) {
+                s2 = it2.next();
+                if (!this.existeChemin(s1.getId(), s2.getId()))
+                    connexe = false;
+            }
         }
 
+        return connexe;
     }
 }
