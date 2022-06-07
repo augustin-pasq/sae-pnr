1e71e1cec918f8ffca4a9acc4d814fd2e735d7dd
Augustin Pasquier <pasquieraugustin@outlook.fr>
2022-06-07 11:43:49 +0200

    add ability to request database

--- Java/src/modele/donnee/UseDatabase.java
+++ Java/src/modele/donnee/UseDatabase.java
@@ -1,7 +1,7 @@
 package modele.donnee;
 
 import java.sql.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class UseDatabase {
 
@@ -21,7 +21,7 @@ public class UseDatabase {
     public static String databaseName = "PNR";
 
     public static void main(String[] args) {
-        selectQuery("SELECT * FROM Obs_Batracien");
+        selectQuery("SELECT lObservateur, COUNT(lObservation) nbObs FROM AObserve GROUP BY lObservateur HAVING nbObs > (SELECT AVG(nbObs) moy FROM (SELECT lObservateur, COUNT(lObservation) nbObs FROM AObserve GROUP BY lObservateur) B);");
     }
 
     /**
@@ -50,22 +50,31 @@ public class UseDatabase {
         return connection;
     }
 
-    public static String selectQuery(String query) {
-        String output = "";
+    public static ArrayList<ArrayList<String>> selectQuery(String query) {
+        ArrayList<ArrayList<String> > output = null;
 
         try {
             Statement stmt = MySQLConnection().createStatement();
             ResultSet rs = stmt.executeQuery(query);
 
-            String[] columnsNames = new String[rs.getMetaData().getColumnCount()];
+            ArrayList<String> columnsNames = new ArrayList<String>();
             for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-                columnsNames[i - 1] = rs.getMetaData().getColumnName(i);
+                columnsNames.add(rs.getMetaData().getColumnName(i));
+            }
+    
+            output = new ArrayList<ArrayList<String> >();
+            output.add(columnsNames);
+
+            
+            while (rs.next()) {
+                ArrayList<String> line = new ArrayList<String>();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    line.add(rs.getString(i));
+                }
+                output.add(line);
             }
 
-            System.out.println(Arrays.toString(columnsNames));
-    
-            while (rs.next()) {
-            }
+            System.out.println(output.toString());
 
             stmt.close();
         } catch (SQLException e) {
