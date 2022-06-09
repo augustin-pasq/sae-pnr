55a9e0f797b853898cc85bf33ccf794279037dbb
finxol <personnal@bottleneck.anonaddy.com>
2022-06-09 10:28:13 +0200

    optimise imports

--- Java/src/vue/Login.java
+++ Java/src/vue/Login.java
@@ -3,7 +3,6 @@ package vue;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
