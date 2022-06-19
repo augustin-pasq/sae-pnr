1a6b4880e542e7ba9498d2cad46cc8cc06587fd0
finxol <personnal@bottleneck.anonaddy.com>
2022-06-20 01:41:43 +0200

    extend InteractivePage

--- Java/src/controleur/GenerateChartController.java
+++ Java/src/controleur/GenerateChartController.java
@@ -1,32 +1,26 @@
 package controleur;
 
-import  modele.donnee.EspeceObservee;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import modele.donnee.EspeceObservee;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
-public class GenerateChartController implements Initializable {
+public class GenerateChartController extends InteractivePage {
 
     ObservableList<EspeceObservee> especeList = FXCollections.observableArrayList(EspeceObservee.values());
-    @FXML
-    private ComboBox<EspeceObservee> especeComboBox;
-
     ObservableList<String> abscisseList = FXCollections.observableArrayList("Période temporelle");
-    @FXML
-    private ComboBox<String> abscisseComboBox;
-
     ObservableList<String> ordoneeList = FXCollections.observableArrayList("Nombre d'individus");
     @FXML
+    private ComboBox<EspeceObservee> especeComboBox;
+    @FXML
+    private ComboBox<String> abscisseComboBox;
+    @FXML
     private ComboBox<String> ordonneeComboBox;
-
-
 
 
     public static void main(String[] args) {
@@ -34,11 +28,11 @@ public class GenerateChartController implements Initializable {
     }
 
 
-
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
         especeComboBox.setItems(especeList);
         abscisseComboBox.setItems(abscisseList);
-        ordonneeComboBox.setItems(ordoneeList); 
+        ordonneeComboBox.setItems(ordoneeList);
     }
 }
