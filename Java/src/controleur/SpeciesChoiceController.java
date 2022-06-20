55c2a25c048291f9b09ed2e6b057928ee2944852
Samuel <samsam.tabor@outlook.fr>
2022-06-20 17:34:19 +0200

    Ajout: - ConsultBatObs (java, fxml, css) - ConsultChouetteObs (java, fxml, css) - ConsultGravelotObs (java, fxml, css) - ConsultHippoObs (java, fxml, css) - ConsultLoutreObs (java, fxml, css)

--- Java/src/controleur/SpeciesChoiceController.java
+++ Java/src/controleur/SpeciesChoiceController.java
@@ -1,9 +1,54 @@
 package controleur;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 /**
  * Controller for the SpeciesChoice page
  *
  * @author Groupe SAE PNR 1D1
  */
 public class SpeciesChoiceController extends InteractivePage {
+
+    @FXML
+    private Button consultObsBatButton;
+
+    @FXML
+    private Button consultObsChouetteButton;
+
+    @FXML
+    private Button consultObsGravelotButton;
+
+    @FXML
+    private Button consultObsHippoButton;
+
+    @FXML
+    private Button consultObsLoutreButton;
+
+
+    @FXML
+    private void consultBatObs (ActionEvent event) {
+        Main.switchScene("ConsultBatObs", consultObsBatButton);
+    }
+
+    @FXML
+    private void consultChouetteObs (ActionEvent event) {
+        Main.switchScene("ConsultChouetteObs", consultObsChouetteButton);
+    }
+
+    @FXML
+    private void consultGravelotObs (ActionEvent event) {
+        Main.switchScene("ConsultGravelotObs", consultObsGravelotButton);
+    }
+
+    @FXML
+    private void consultHippoObs (ActionEvent event) {
+        Main.switchScene("ConsultHippoObs", consultObsHippoButton);
+    }
+
+    @FXML
+    private void consultLoutreObs (ActionEvent event) {
+        Main.switchScene("ConsultLoutreObs", consultObsLoutreButton);
+    }
 }
