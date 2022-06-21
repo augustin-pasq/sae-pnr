c1c747c24dd16933684c5ea0aead11e1146cee4a
liam-leny <liamleny@protonmail.com>
2022-06-21 18:14:21 +0200

    Adding controllers to fill the ComboBoxes

---
+++ Java/src/controleur/DataLoutreController.java
@@ -0,0 +1,28 @@
+package controleur;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class DataLoutreController extends InteractivePage {
+
+    ObservableList<String> indiceList = FXCollections.observableArrayList("Positif", "Négatif", "Non prospection");
+
+    @FXML
+    private ComboBox<String> indiceComboBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle ressourceBundle) {
+        super.initialize(url, ressourceBundle);
+        this.isAdmin = true;
+        indiceComboBox.setItems(indiceList);
+        
+
+    }
+}
