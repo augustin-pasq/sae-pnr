5371e2cb513f91927f4515f8cfa4bc433825c723
finxol <personnal@bottleneck.anonaddy.com>
2022-06-22 05:31:23 +0200

    refactor

--- Java/src/controleur/NoInternetException.java
+++ Java/src/controleur/NoInternetException.java
@@ -1,6 +1,6 @@
 package controleur;
 
-public class NoInternetException extends Exception{
+public class NoInternetException extends Exception {
     public NoInternetException(String msg) {
         super(msg);
     }
