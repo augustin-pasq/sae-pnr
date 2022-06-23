c33fb2c7fd3e3666d22d4623dd47a474de84040f
RaavenBZH <emalivet56@gmail.com>
2022-06-23 09:48:54 +0200

    javadoc

--- Java/src/controleur/HomePageController.java
+++ Java/src/controleur/HomePageController.java
@@ -8,19 +8,40 @@ import javafx.scene.control.Button;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Controller for the HomePage page
+ */
 public class HomePageController implements Initializable {
+
+    /**
+     * The button to go to the ConsultData pages
+     */
     @FXML
     public Button consulter;
 
+    /**
+     * The button to go to the Edit pages
+     */
     @FXML
     public Button modifier;
 
+    /**
+     * The button to go to the Data pages
+     */
     @FXML
     public Button saisir;
 
+    /**
+     * The button to logout
+     */
     @FXML
     public Button logout;
 
+    /**
+     * Move the current page to the correct page, which is determined by the button clicked
+     * 
+     * @param event the event
+     */
     public void move(final ActionEvent event) {
         Button target = (Button) event.getSource();
         switch (target.getId()) {
@@ -38,6 +59,10 @@ public class HomePageController implements Initializable {
         }
     }
 
+    /**
+     * Inherited method from Initializable
+     * @see javafx.fxml.Initializable
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
