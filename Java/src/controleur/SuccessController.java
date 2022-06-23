f8b4daabddabeb1e90aca399e87c5fb7fd111a88
RaavenBZH <emalivet56@gmail.com>
2022-06-23 09:31:33 +0200

    javadoc

--- Java/src/controleur/SuccessController.java
+++ Java/src/controleur/SuccessController.java
@@ -3,8 +3,15 @@ package controleur;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Controller for the succes message popup
+ */
 public class SuccessController extends InteractivePage {
 
+    /**
+     * Inherited method from Initializable
+     * @see javafx.fxml.Initializable
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.homeButton.setOnAction(super::goHome);
